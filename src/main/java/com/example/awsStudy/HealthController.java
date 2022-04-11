4a58e6ed010f09d2e386e9a4df1464e4fa0142c4
JahyunBaek <backjahyun91@gmail.com>
2022-04-11 17:15:19 +0900

    comiit

--- src/main/java/com/example/awsStudy/HealthController.java
+++ src/main/java/com/example/awsStudy/HealthController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping
     public String healthCheck(){
-        return "health ok1";
+        return "health ok12";
     }
 }
